b8ce4f3aeeed55e01489a8c35bafc09fbebaa1dc
Kurt McAlpine <kurt@linux.com>
2025-05-24 16:34:35 +1200

    wip

--- .dagger/main.go
+++ .dagger/main.go
@@ -483,7 +483,6 @@ func (m *ValkeyClusterOperator) CreateGitHubRelease(
 
 	return m.GhCliContainer().
 		WithDirectory("/manifest", manifestDir).
-		WithExec([]string{"false"}).
 		WithSecretVariable("GH_TOKEN", ghToken).
 		WithExec([]string{"gh", "release", "create", "v1.2.3", "/manifest/install.yaml"}).Stdout(ctx)
 
