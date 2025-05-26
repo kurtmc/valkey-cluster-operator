4e6c3e2ae24826119d6e9ce40437fd874918aaca
Kurt McAlpine <kurt@linux.com>
2025-05-26 16:19:04 +1200

    Remove build-and-deploy.sh script

--- build-and-deploy.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-NEW_TAG="$(dagger call publish-docker)"
-kubectl config use-context halter-sandbox && aws-vault exec halter-sandbox -- make deploy IMG="quay.io/kurtmcalpine/valkey-cluster-operator:${NEW_TAG}"
